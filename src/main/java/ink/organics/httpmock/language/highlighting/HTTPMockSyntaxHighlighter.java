59ab5b9a59cee9457ab6f6ffaf02cd6a61d98170
organics <organics2015@outlook.com>
2022-05-11 14:41:23 +0800

    update

---
+++ src/main/java/ink/organics/httpmock/language/highlighting/HTTPMockSyntaxHighlighter.java
@@ -0,0 +1,62 @@
+package ink.organics.httpmock.language.highlighting;
+
+import com.intellij.lexer.Lexer;
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
+import com.intellij.openapi.editor.HighlighterColors;
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.fileTypes.SyntaxHighlighterBase;
+import com.intellij.psi.TokenType;
+import com.intellij.psi.tree.IElementType;
+import ink.organics.httpmock.language.lexer.HTTPMockLexerAdapter;
+import ink.organics.httpmock.language.psi.HTTPMockTypes;
+import org.jetbrains.annotations.NotNull;
+
+import static com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributesKey;
+
+public class HTTPMockSyntaxHighlighter extends SyntaxHighlighterBase {
+
+    public static final TextAttributesKey SEPARATOR =
+            createTextAttributesKey("SIMPLE_SEPARATOR", DefaultLanguageHighlighterColors.OPERATION_SIGN);
+    public static final TextAttributesKey KEY =
+            createTextAttributesKey("SIMPLE_KEY", DefaultLanguageHighlighterColors.KEYWORD);
+    public static final TextAttributesKey VALUE =
+            createTextAttributesKey("SIMPLE_VALUE", DefaultLanguageHighlighterColors.STRING);
+    public static final TextAttributesKey COMMENT =
+            createTextAttributesKey("SIMPLE_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
+    public static final TextAttributesKey BAD_CHARACTER =
+            createTextAttributesKey("SIMPLE_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER);
+
+
+    private static final TextAttributesKey[] BAD_CHAR_KEYS = new TextAttributesKey[]{BAD_CHARACTER};
+    private static final TextAttributesKey[] SEPARATOR_KEYS = new TextAttributesKey[]{SEPARATOR};
+    private static final TextAttributesKey[] KEY_KEYS = new TextAttributesKey[]{KEY};
+    private static final TextAttributesKey[] VALUE_KEYS = new TextAttributesKey[]{VALUE};
+    private static final TextAttributesKey[] COMMENT_KEYS = new TextAttributesKey[]{COMMENT};
+    private static final TextAttributesKey[] EMPTY_KEYS = new TextAttributesKey[0];
+
+
+    @Override
+    public @NotNull Lexer getHighlightingLexer() {
+        return new HTTPMockLexerAdapter();
+    }
+
+    @Override
+    public TextAttributesKey @NotNull [] getTokenHighlights(IElementType tokenType) {
+        if (tokenType.equals(HTTPMockTypes.TODO)) {
+            return SEPARATOR_KEYS;
+        }
+        if (tokenType.equals(HTTPMockTypes.TODO)) {
+            return KEY_KEYS;
+        }
+        if (tokenType.equals(HTTPMockTypes.TODO)) {
+            return VALUE_KEYS;
+        }
+        if (tokenType.equals(HTTPMockTypes.COMMENT)) {
+            return COMMENT_KEYS;
+        }
+        if (tokenType.equals(TokenType.BAD_CHARACTER)) {
+            return BAD_CHAR_KEYS;
+        }
+        return EMPTY_KEYS;
+    }
+}
